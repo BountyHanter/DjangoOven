f6b6b33d631d5b31d4ca3ad71c0c998550401ece
denis <denison99@mail.ru>
2026-02-16 22:05:26 +0500

    fix pagination

--- config/settings.py
+++ config/settings.py
@@ -100,8 +100,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 9,
+    "DEFAULT_PAGINATION_CLASS": "config.utils.pagination.DefaultPagination",
     "DATETIME_FORMAT": "%Y-%m-%dT%H:%M:%S.%f%z",
     "USE_TZ": True,
 }
