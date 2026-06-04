b55b123114148ea06e8a903c6de2db844e2510ec
denis <denison99@mail.ru>
2026-06-04 14:59:17 +0500

    add logs

--- main_app/serializers/email.py
+++ main_app/serializers/email.py
@@ -1,5 +1,8 @@
+import logging
+
 from rest_framework import serializers
 
+logger = logging.getLogger(__name__)
 
 class EmailRequestSerializer(serializers.Serializer):
     phone = serializers.CharField(min_length=5, max_length=20)
@@ -15,6 +18,7 @@ class EmailRequestSerializer(serializers.Serializer):
         value = value.strip()
 
         if not value.startswith("https://kamini-melnika.ru/"):
+            logger.warning(f"Ошибка отправки email. Не верная ссылка: {value}")
             raise serializers.ValidationError("Ссылка должна быть с kamini-melnika.ru")
 
         return value
